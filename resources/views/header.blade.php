15c4ff93108ede65af642aebf2a03e5f0d9f98d2
MartinaBasile <martinaleandribasile@gmail.com>
2022-10-27 17:09:15 +0200

    links dinamici

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,5 +1,24 @@
+@php
+    $links = [
+        'link1' => [
+            'url' => '/',
+            'name' => 'Home',
+        ],
+        'link2' => [
+            'url' => '/presentazione',
+            'name' => 'Presentazione',
+        ],
+        'link3' => [
+            'url' => '/lista',
+            'name' => 'Lista',
+        ],
+    ];
+@endphp
+
+
 <header class="d-flex gap-3 my-3">
-    <a href="{{ url('/') }}">Home</a>
-    <a href="{{ url('/presentazione') }}">Presentazione</a>
-    <a href="{{ url('/lista') }}">Lista</a>
+    @foreach ($links as $link)
+        <a href="{{ url($link['url']) }}">{{ $link['name'] }}</a>
+    @endforeach
+
 </header>
